91513d7d8e8c7a5e661d3ba6dbcbc1ee600bc82e
Marcelo M. Eler <marceloeler@gmail.com>
2021-10-07 14:37:57 -0300

    nova refatoracao

--- src/main/java/each/ach/si/Triangulo.java
+++ src/main/java/each/ach/si/Triangulo.java
@@ -2,6 +2,12 @@ package each.ach.si;
 
 public class Triangulo {
 
+    public static boolean ladoInvalido(int A, int B, int C){
+        if (A<=0 || B<=0 || C<0)
+            return true;
+        return false;
+    }
+
     public static boolean formaTriangulo(int A, int B, int C){
         if (A<B+C || B<A+C || C<A+B)
             return true;
@@ -9,9 +15,7 @@ public class Triangulo {
     }
 
     public static TipoTriangulo classificaTriangulo(int A, int B, int C){
-        if (A<=0 || B<=0 || C<0)
-            return TipoTriangulo.INVALIDO;
-        if (!formaTriangulo(A,B,C))
+        if (!formaTriangulo(A,B,C) || ladoInvalido(A,B,C))
             return TipoTriangulo.INVALIDO;
         if (A==B && B==C)
             return TipoTriangulo.ESCALENO;
